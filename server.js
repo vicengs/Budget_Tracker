8ccc8cf24867147f4dec5f6bdc72a3adfda7104e
Vicente Garcia <vicen1@hotmail.com>
2022-06-18 11:58:33 -0500

    Solve port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 // Add port (27017) to deploy
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/:27017/budget-tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budget-tracker";
 
 const app = express();
 
